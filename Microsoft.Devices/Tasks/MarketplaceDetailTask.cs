c7bbe9c45bd9e99e4810299671c3c689111f1ae8
Aranda Morrison <aranda_morrison@yahoo.com.au>
2012-10-22 22:55:17 +0800

    Add some classes in Microsoft.Phone.Tasks to Android assembly

---
+++ Microsoft.Devices/Tasks/MarketplaceDetailTask.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Net;
+
+namespace Microsoft.Phone.Tasks
+{
+    /// <summary>
+    /// Android implementaion of the MarketplaceDetailTask.
+    /// </summary>
+    public sealed class MarketplaceDetailTask
+    {
+        // Android package name, eg "com.rovio.angrybirds"
+        public string ContentIdentifier { get; set; }
+
+        // Ignored by this implementation
+        public MarketplaceContentType ContentType { get; set; }
+
+        public void Show()
+        {
+            Intent intent = new Intent(Intent.ActionView);
+            intent.SetData(Uri.Parse("market://details?id=" + ContentIdentifier));
+            intent.SetFlags(ActivityFlags.NewTask);
+            Application.Context.StartActivity(intent);
+        }
+    }
+}
---
+++ Microsoft.Devices/Tasks/MarketplaceReviewTask.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Net;
+
+namespace Microsoft.Phone.Tasks
+{
+    /// <summary>
+    /// Android implementaion of the MarketplaceReviewTask.
+    /// </summary>
+    public sealed class MarketplaceReviewTask
+    {
+        public void Show()
+        {
+            Intent intent = new Intent(Intent.ActionView);
+            intent.SetData(Uri.Parse("market://details?id=" + Application.Context.PackageName));
+            intent.SetFlags(ActivityFlags.NewTask);
+            Application.Context.StartActivity(intent);
+        }
+    }
+}
